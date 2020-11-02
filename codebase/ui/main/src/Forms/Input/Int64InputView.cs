384e6e7946d0badcdf309740c10f292389b8f1b4
ivaylo5ev <ivaylo5ev@gmail.com>
2020-11-02 10:31:50 +0200

    * Forest.UI: added nullable counterparts for some input fields views

--- codebase/ui/main/src/Forms/Input/Int64InputView.cs
+++ codebase/ui/main/src/Forms/Input/Int64InputView.cs
@@ -4,7 +4,7 @@
     public sealed class Int64InputView : FormInputView<long>
     {
         private const string Name = "Int64Input";
-        
+
         internal Int64InputView(long model) : base(model) { }
         internal Int64InputView() : this(0L) { }
     }
