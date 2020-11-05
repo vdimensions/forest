796a6bacb6286a04d64988dbd1909d6c7d41638c
ivaylo5ev <ivaylo5ev@gmail.com>
2020-11-06 00:16:19 +0200

    - validation rule enum changes

--- codebase/ui/main/src/Forms/Validation/ValidationRule.cs
+++ codebase/ui/main/src/Forms/Validation/ValidationRule.cs
@@ -1,13 +1,30 @@
+using System;
+using System.Runtime.Serialization;
+
 namespace Forest.UI.Forms.Validation
 {
+    [Serializable, DataContract]
     public enum ValidationRule
     {
+        [EnumMember(Value = "required")]
         Required = 0,
+        
+        [EnumMember(Value = "maxLength")]
         MaxLength,
+        
+        [EnumMember(Value = "minLength")]
         MinLength,
+        
+        [EnumMember(Value = "maxValue")]
         MaxValue,
+        
+        [EnumMember(Value = "minValue")]
         MinValue,
+        
+        [EnumMember(Value = "regex")]
         Regex,
+        
+        [EnumMember(Value = "compare")]
         Compare,
     }
 }
