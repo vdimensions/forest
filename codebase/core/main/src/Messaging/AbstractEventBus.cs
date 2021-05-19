0327955a8e005b8c09903af7a9d7ec8fcfc55382
ivaylo5ev <ivaylo5ev@gmail.com>
2021-05-19 13:21:26 +0300

    - messaging structs now support serialization, no longer nested

--- codebase/core/main/src/Messaging/AbstractEventBus.cs
+++ codebase/core/main/src/Messaging/AbstractEventBus.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using Axle;
 using Axle.Collections;
-using Axle.Extensions.Object;
-using Forest.Messaging.Propagating;
 
 namespace Forest.Messaging
 {
@@ -15,78 +12,6 @@ namespace Forest.Messaging
         protected sealed class SubscriptionHandlerSet : HashSet<ISubscriptionHandler>
         {
             public SubscriptionHandlerSet() : base(new LensingEqualityComparer<ISubscriptionHandler, IView>(sh => sh.Receiver, new ReferenceEqualityComparer<IView>())) { }
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        protected readonly struct DistributionData : IEquatable<DistributionData>
-        {
-            public DistributionData(params string[] topics) : this()
-            {
-                PropagationTargets = null;
-                Topics = topics;
-            }
-            public DistributionData(PropagationTargets propagationTargets) : this()
-            {
-                PropagationTargets = propagationTargets;
-                Topics = null;
-            }
-
-            public bool Equals(DistributionData other)
-            {
-                return Nullable.Equals(PropagationTargets, other.PropagationTargets) 
-                    && Topics.SequenceEqual(other.Topics);
-            }
-            public override bool Equals(object obj) => obj is DistributionData other && Equals(other);
-
-            public override int GetHashCode()
-            {
-                return this.CalculateHashCode(PropagationTargets, this.CalculateHashCode(Topics));
-            }
-            
-            public PropagationTargets? PropagationTargets { get; }
-            public string[] Topics { get; }
-        }
-        
-        [StructLayout(LayoutKind.Sequential)]
-        protected readonly struct Letter : IComparable<Letter>, IEquatable<Letter>
-        {
-            public Letter(IView sender, object message, long timestamp, DistributionData distributionData)
-            {
-                Sender = sender;
-                Message = message;
-                Timestamp = timestamp;
-                DistributionData = distributionData;
-            }
-
-            public int CompareTo(Letter other) => Timestamp.CompareTo(other.Timestamp);
-
-            public override bool Equals(object obj) => obj is Letter other && Equals(other);
-
-            public bool Equals(Letter other)
-            {
-                return ReferenceEquals(Sender, other.Sender) 
-                    && Equals(Message, other.Message) 
-                    && Equals(DistributionData, other.DistributionData) 
-                    && Timestamp == other.Timestamp;
-            }
-
-            public override int GetHashCode()
-            {
-                unchecked
-                {
-                    var hashCode = 848193896;
-                    hashCode = hashCode * -1521134295 + (Sender == null ? 0 : Sender.GetHashCode());
-                    hashCode = hashCode * -1521134295 + Message.GetHashCode();
-                    hashCode = hashCode * -1521134295 + DistributionData.GetHashCode();
-                    hashCode = hashCode * -1521134295 + Timestamp.GetHashCode();
-                    return hashCode;
-                }
-            }
-
-            public IView Sender { get; }
-            public object Message { get; }
-            public DistributionData DistributionData { get; }
-            private long Timestamp { get; }
         }
         
         protected readonly IDictionary<Letter, SubscriptionHandlerSet> MessageHistory =
---
+++ codebase/core/main/src/Messaging/Letter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Forest.Messaging
+{
+    #if NETSTANDARD2_0_OR_NEWER || NETFRAMEWORK
+    [Serializable]
+    #endif
+    [StructLayout(LayoutKind.Sequential)]
+    internal readonly struct Letter : IComparable<Letter>, IEquatable<Letter>
+    {
+        public Letter(IView sender, object message, long timestamp, DistributionData distributionData)
+        {
+            Sender = sender;
+            Message = message;
+            Timestamp = timestamp;
+            DistributionData = distributionData;
+        }
+
+        public int CompareTo(Letter other) => Timestamp.CompareTo(other.Timestamp);
+
+        public override bool Equals(object obj) => obj is Letter other && Equals(other);
+
+        public bool Equals(Letter other)
+        {
+            return ReferenceEquals(Sender, other.Sender) 
+                   && Equals(Message, other.Message) 
+                   && Equals(DistributionData, other.DistributionData) 
+                   && Timestamp == other.Timestamp;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = 848193896;
+                hashCode = hashCode * -1521134295 + (Sender == null ? 0 : Sender.GetHashCode());
+                hashCode = hashCode * -1521134295 + Message.GetHashCode();
+                hashCode = hashCode * -1521134295 + DistributionData.GetHashCode();
+                hashCode = hashCode * -1521134295 + Timestamp.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public IView Sender { get; }
+        public object Message { get; }
+        public DistributionData DistributionData { get; }
+        private long Timestamp { get; }
+    }
+}
