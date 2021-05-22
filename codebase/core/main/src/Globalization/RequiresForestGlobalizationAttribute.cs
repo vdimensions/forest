af149b00a3f64c99a884ed0b1816094b387b54d9
ivaylo5ev <ivaylo5ev@gmail.com>
2021-05-22 19:13:30 +0300

    - exposed attribute to make modules depend on forest globalization

---
+++ codebase/core/main/src/Globalization/RequiresForestGlobalizationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Axle.Modularity;
+
+namespace Forest.Globalization
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Interface, Inherited = true, AllowMultiple = false)]
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    public sealed class RequiresForestGlobalizationAttribute : RequiresAttribute
+    {
+        public RequiresForestGlobalizationAttribute() : base(typeof(ForestGlobalizationModule))
+        {
+        }
+    }
+}
